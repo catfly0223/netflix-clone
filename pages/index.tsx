9af371e1b99b6bd869f72b897f7efa7e6eae5098
Itsuki Yashina <iyashina0223@gmail.com>
2023-11-02 14:25:25 +0900

    checkpoint

---
+++ pages/index.tsx
@@ -0,0 +1,45 @@
+import { NextPageContext } from 'next/types'
+import { getSession } from 'next-auth/react'
+import Navbar from '@/components/Navbar';
+import Billboard from '@/components/Billboard';
+import MovieList from '@/components/MovieList';
+import useMovieList from '@/hooks/useMovieList';
+import useFavorites from '@/hooks/useFavorites';
+import InfoModal from '@/components/InfoModal';
+import useInfoModal from '@/hooks/useInfoModal';
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
+
+export default function Home() {
+  const { data: movies = [] } = useMovieList();
+  const { data: favorites = [] } = useFavorites();
+  const { isOpen, closeModal } = useInfoModal();
+
+  return (
+    <>
+      <InfoModal visible={isOpen} onClose={closeModal} />
+      <Navbar />
+      <Billboard />
+      <div className="pb-40 text-black">
+        <MovieList title="Trending Now" data={movies} />
+        <MovieList title="My List" data={favorites} />
+      </div>
+
+    </>
+  )
+}
---
+++ pages/test.tsx
@@ -0,0 +1,7 @@
+const MyPage = () => {
+    return(
+        <h1 className="text-2xl">Hello New Page</h1>
+    )
+}
+
+export default MyPage;
